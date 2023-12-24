0260aa466a6153bf0610ab67e0bf2498f5a5fed6
buyfakett <buyfakett@vip.qq.com>
2023-12-24 15:37:38 +0800

    增加七牛云

---
+++ tt_util/qiniu_util.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import logging
+import os
+import uuid
+
+from tt_util.uuid_util import edit_uuid
+from tt_util.yaml_util import read_yaml
+
+from qiniu import put_file, Auth, BucketManager
+
+from api.base import resp_400
+
+
+class QiniuFunction(object):
+    def __init__(self, access_key=read_yaml('access_key', 'config'), secret_key=read_yaml('secret_key', 'config'),
+                 bucket=str(read_yaml('bucket_name', 'config'))):
+        self.bucket = bucket
+        self.q = Auth(access_key, secret_key)
+        self.domain = read_yaml('domain', 'config')
+
+    def upload_file(self, local_file, remote_file):
+        # 上传后保存的文件名
+        token = self.q.upload_token(self.bucket, remote_file, 999999)
+        ret, info = put_file(token, remote_file, local_file, version='v2')
+        return ret, info
+
+    def download_file(self, remote_file):
+        # 有两种方式构造base_url的形式
+        base_url = 'https://%s/%s' % (self.domain, remote_file)
+        # private_url = self.q.private_download_url(base_url, expires=3600)
+        return base_url
+
+    def delete_file(self, remote_file):
+        bucket_manager = BucketManager(self.q)
+        ret, info = bucket_manager.delete(self.bucket, remote_file)
+        return ret, info
