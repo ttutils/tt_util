f6f3c97a6a84cec2f1e300c81974c03a53866a93
buyfakett <buyfakett@vip.qq.com>
2024-01-07 16:38:31 +0800

    增加修改yaml

--- tt_util/yaml_util.py
+++ tt_util/yaml_util.py
@@ -9,9 +9,6 @@ def read_yaml(key: str, filename: str):
     :param filename: string     配置文件的名字
     :return         *           要读取的值
     """
-    if not os.path.exists(os.getcwd() + '/config/' + filename + '.yaml'):
-        logging.error(f"config目录下没有{filename}.yaml文件，请添加文件！！！")
-        raise SystemExit(0)
     try:
         with open(os.getcwd() + '/config/' + filename + '.yaml', encoding="utf-8") as f:
             value = yaml.load(stream=f, Loader=yaml.FullLoader)
@@ -19,6 +16,33 @@ def read_yaml(key: str, filename: str):
     except:
         logging.error(f"{filename}.yaml文件中没有{key}配置，请检查配置文件！！！")
         raise SystemExit(0)
+
+
+# 修改yaml
+def update_yaml(key, new_value, filename):
+    """
+    修改yaml
+    :param key:     string      要读取的值的名称
+    :param new_value: string     修改成的值
+    :param filename: string     配置文件的名字
+    """
+    try:
+        with open(os.getcwd() + '/config/' + filename + '.yaml', 'r') as file:
+            yaml_data = yaml.safe_load(file)
+
+        if yaml_data is not None:
+            yaml_data[key] = new_value
+
+            with open(os.getcwd() + '/config/' + filename + '.yaml', 'w') as file:
+                yaml.safe_dump(yaml_data, file, default_flow_style=False)
+
+            logging.info(f"YAML文件 '{filename}' 已成功更新")
+        else:
+            print(f"YAML文件 '{filename}' 是空的")
+    except FileNotFoundError:
+        print(f"'{filename}' 找啊不到该文件")
+    except Exception as e:
+        print(f"出现错误: {e}")
 
 
 # 追加写入yaml
