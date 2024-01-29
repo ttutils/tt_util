c0a8289b4884398ebda0632bf57d213c930c34e5
buyfakett <buyfakett@vip.qq.com>
2024-01-29 17:20:37 +0800

    修改名字和注释

--- tt_util/docker_util.py
+++ tt_util/docker_util.py
@@ -1,6 +1,3 @@
-# -*- coding: utf-8 -*-            
-# @Author : buyfakett
-# @Time : 2024/1/25 20:02
 import docker
 
 
@@ -8,10 +5,16 @@ class DockerController:
     def __init__(self, base_url: str):
         self.client = docker.DockerClient(base_url=base_url)
 
-    def running_containers(self):
+    def list_running_containers(self):
+        """
+        查询所有的容器
+        """
         return self.client.containers.list()
 
-    def list_running_containers(self):
+    def list_running_object_containers(self):
+        """
+        查询所有的容器，已List返回
+        """
         container_list = []
         for container in self.client.containers.list():
             container_list.append({
@@ -23,13 +26,25 @@ class DockerController:
         return container_list
 
     def inspect_container(self, container_id):
+        """
+        获取单个容器
+        """
         return self.client.containers.get(container_id)
 
     def run_container(self, image, command=None, detach=True):
+        """
+        启动容器
+        """
         return self.client.containers.run(image, command, detach=detach)
 
     def stop_container(self, container_id):
+        """
+        暂停容器
+        """
         self.client.containers.get(container_id).stop()
 
     def remove_container(self, container_id):
+        """
+        删除容器
+        """
         self.client.containers.get(container_id).remove()
